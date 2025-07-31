f2b5cfb1ec8ad92f99fda73698d5fa3449d09e9c
itushnk <itamacoh@gmail.com>
2025-07-31 16:24:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import telebot
+import os
+import time
 
 BOT_TOKEN = "8371104768:AAE8GYjVBeF0H4fqOur9tMLe4_D4laCBRsk"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "שלום! הבוט שלך מוכן ומחכה לפקודות 😊")
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, f"קיבלת: {message.text}")
+    bot.reply_to(message, "הבוט פעיל!")
 
 print("הבוט מופעל...")
 bot.infinity_polling()
